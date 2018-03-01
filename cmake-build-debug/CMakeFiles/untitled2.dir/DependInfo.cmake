cdaee9cf8b13381cf1c9a02d198bff5b5b36bd98
Antony Shanin <holyshine@yahoo.com>
2018-03-01 21:06:03 +0200

    upd

--- cmake-build-debug/CMakeFiles/untitled2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/holyshining/CLionProjects/untitled2/main.cpp" "/Users/holyshining/CLionProjects/untitled2/cmake-build-debug/CMakeFiles/untitled2.dir/main.cpp.o"
+  "/Users/holyshining/Desktop/Projects/pur-charm-ghc/main.cpp" "/Users/holyshining/Desktop/Projects/pur-charm-ghc/cmake-build-debug/CMakeFiles/untitled2.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <php>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    struct ride{
+        int xStart, yStart, xFinish, yFinish, earStart, latFinish;
+    };
+
+    int rows, colums, vehicles, rides, bonus, steps, currentSteps;
+    ride a[10];
+
+    cin>>rows>>colums>>vehicles>>rides>>bonus>>steps;
+    for (int i = 0; i < rides; ++i) {
+        cin>>a[i].xStart>>a[i].yStart>>a[i].xFinish>>a[i].yFinish>>a[i].earStart>>a[i].latFinish;
+    }
+
+    cout<<"1 0"<<endl<<"2 2 1";
+
+    ifstream
+
+
+
     return 0;
 }
